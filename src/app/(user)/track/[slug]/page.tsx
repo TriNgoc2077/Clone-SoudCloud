45e6d3311660486a687e1cf0205349bb0414fa5c
Cao Nguyen Tri Ngoc <caonguyentringoc@gmail.com>
2025-05-15 16:58:25 +0700

    chores

--- src/app/(user)/track/[slug]/page.tsx
+++ src/app/(user)/track/[slug]/page.tsx
@@ -6,7 +6,8 @@ import type { Metadata, ResolvingMetadata } from 'next'
 type Props = {
   params:{ slug: string }
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-}
+}  
+
  
 export async function generateMetadata(
   { params, searchParams }: Props,
