b255b2c16cbf73bb7bed04fd3eb8d51da2a0216c
Cao Tri Ngoc <tringoc2077@gmail.com>
2025-05-24 23:08:08 +0700

    loading playlist page

--- src/app/(user)/playlist/loading.tsx
+++ src/app/(user)/playlist/loading.tsx
@@ -1,3 +1,29 @@
+// Loading.tsx
+import { Box, CircularProgress, Typography } from '@mui/material';
+
 export default function Loading() {
-    return <p>Loading...</p>
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: 'linear-gradient(to bottom right, #ffe0f0, #fff0f5)',
+      }}
+    >
+      <CircularProgress
+        sx={{ color: '#ff69b4', mb: 2 }}
+        size={60}
+        thickness={4}
+      />
+      <Typography
+        variant="h5"
+        sx={{ color: '#ff69b4', fontWeight: 'bold', fontFamily: 'sans-serif' }}
+      >
+        Loading your lovely playlists...
+      </Typography>
+    </Box>
+  );
 }
